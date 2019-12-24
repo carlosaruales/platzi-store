cae40249e4a7e4e746177f108dd801269c2ab3bd
Andres ruales <carlosa.ruales@gmail.com>
2019-12-23 20:31:40 -0500

    Shared module y core module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,8 @@ import { BannerComponent } from './componentes/banner/banner.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { HomeRoutingModule } from './home.routing.module';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +13,8 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     HomeRoutingModule,
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 

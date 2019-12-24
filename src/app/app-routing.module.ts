59c9084dea825f41e23e154e259cc6c8117fbcef
Andres ruales <carlosa.ruales@gmail.com>
2019-12-23 21:30:16 -0500

    guardianes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AdminGuard } from './admin.guard';
 
 
 const routes: Routes = [
@@ -19,7 +20,7 @@ const routes: Routes = [
       },
       { path: 'products', component: ProductsComponent },
       { path: 'products/:id', component: ProductDetailComponent},
-      { path: 'contact', component: ContactComponent }
+      { path: 'contact', component: ContactComponent, canActivate: [AdminGuard] }
     ]
   },
   { path: 'demo', component: DemoComponent },

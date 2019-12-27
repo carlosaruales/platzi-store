dc4397dcee3e6b66a1dccf917e6102b838664b76
Andres ruales <carlosa.ruales@gmail.com>
2019-12-26 21:54:56 -0500

    cambios en proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { ProductsComponent } from './products/products.component';
-import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 import { AdminGuard } from './admin.guard';
 
@@ -18,9 +15,16 @@ const routes: Routes = [
         path: 'home',
         loadChildren: () =>  import('./home/home.module').then(m => m.HomeModule)
       },
-      { path: 'products', component: ProductsComponent },
-      { path: 'products/:id', component: ProductDetailComponent},
-      { path: 'contact', component: ContactComponent, canActivate: [AdminGuard] }
+      {
+        path: 'products',
+        canActivate: [AdminGuard],
+        loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+      },
+      {
+        path: 'contact',
+        canActivate: [AdminGuard],
+        loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
+      }
     ]
   },
   { path: 'demo', component: DemoComponent },

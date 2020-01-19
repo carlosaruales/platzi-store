2323c30bbcec91d6ab7f742d98a904994c7cdbc2
Andres ruales <carlosa.ruales@gmail.com>
2020-01-19 11:21:23 -0500

    programación reactiva

--- src/app/product/components/product/product.component.ts
+++ src/app/product/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../../product.model';
+import { CartService } from 'src/app/core/services/cart.service';
 
 @Component({
   selector: 'app-product',
@@ -11,10 +12,15 @@ export class ProductComponent {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<string> = new EventEmitter();
 
+  constructor(private cartService: CartService) {
+
+  }
+
   today = new Date();
 
   addCart() {
     console.log('añadir al carrito');
-    this.productClicked.emit(this.product.id);
+    // this.productClicked.emit(this.product.id);
+    this.cartService.addCart(this.product);
   }
 }

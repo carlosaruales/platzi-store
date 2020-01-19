20f472eee134cb27877e685c055ef38dc61e642b
Andres ruales <carlosa.ruales@gmail.com>
2020-01-18 19:42:59 -0500

    validaciones personalizadas

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductsService } from 'src/app/core/services/products/products.service';
 import { Router } from '@angular/router';
+import { MyValidators } from './../../../utils/validators';
 
 
 @Component({
@@ -26,7 +27,7 @@ export class FormProductComponent implements OnInit {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
-      price: ['', [Validators.required]],
+      price: ['', [Validators.required, MyValidators.isPriceValid]],
       image: [''],
       description: ['', [Validators.required]],
     });
@@ -45,6 +46,10 @@ export class FormProductComponent implements OnInit {
       console.log(this.form.errors);
     }
     console.log(this.form.value);
+  }
+
+  get priceField() {
+    return this.form.get('price');
   }
 
 }

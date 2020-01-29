845270ce49dd6d90050ad4f1342465f507b48d1c
Andres ruales <carlosa.ruales@gmail.com>
2020-01-28 20:40:51 -0500

    uso de firebase, configuracion y registro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url_api: 'http://platzi-store.herokuapp.com'
+  url_api: 'http://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyChcb7WpdsLLRr1EM48MLn1Jym42LLifyw',
+    authDomain: 'platzi-store-18f69.firebaseapp.com',
+    databaseURL: 'https://platzi-store-18f69.firebaseio.com',
+    projectId: 'platzi-store-18f69',
+    storageBucket: 'platzi-store-18f69.appspot.com',
+    messagingSenderId: '689382306878',
+    appId: '1:689382306878:web:331d3265ee875663764d6f'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  url_api: 'http://platzi-store.herokuapp.com'
+  url_api: 'http://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyChcb7WpdsLLRr1EM48MLn1Jym42LLifyw',
+    authDomain: 'platzi-store-18f69.firebaseapp.com',
+    databaseURL: 'https://platzi-store-18f69.firebaseio.com',
+    projectId: 'platzi-store-18f69',
+    storageBucket: 'platzi-store-18f69.appspot.com',
+    messagingSenderId: '689382306878',
+    appId: '1:689382306878:web:331d3265ee875663764d6f'
+  }
 };
 
 /*

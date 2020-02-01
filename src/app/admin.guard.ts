00b7facf192130d685716e4d52b794c624655dad
Andres ruales <carlosa.ruales@gmail.com>
2020-02-01 16:01:59 -0500

    subir una imagen al storage

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable} from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './core/services/auth.service';
 import { map, tap } from 'rxjs/operators';
@@ -9,16 +9,21 @@ import { map, tap } from 'rxjs/operators';
 })
 export class AdminGuard implements CanActivate {
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService,
+              private router: Router ) {
 
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.auth.hasUser().pipe(
-      tap(user => console.log(user)),
-      map(user => user !== null)
+    return this.auth.hasUser().pipe( 
+      map(user => user !== null),
+      tap(hasUser => {
+        if (!hasUser) {
+          this.router.navigate(['/auth/login']);
+        }
+      })
     );
   }
 }
--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ProductsService } from 'src/app/core/services/products/products.service';
 import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
+
+import { finalize } from 'rxjs/operators';
+
 import { MyValidators } from './../../../utils/validators';
 
+import { ProductsService } from './../../../core/services/products/products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -13,14 +18,49 @@ import { MyValidators } from './../../../utils/validators';
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
 
-  constructor(private formBuilder: FormBuilder,
-              private productService: ProductsService,
-              private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router,
+    private storage: AngularFireStorage
+  ) {
     this.buildForm();
   }
 
   ngOnInit() {
+  }
+
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product)
+      .subscribe((newProduct) => {
+        console.log(newProduct);
+        this.router.navigate(['./admin/products']);
+      });
+    }
+  }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const name = 'image.png';
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+    .pipe(
+      finalize(() => {
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe(url => {
+          console.log(url);
+          this.form.get('image').setValue(url);
+        });
+      })
+    )
+    .subscribe();
   }
 
   private buildForm() {
@@ -31,21 +71,6 @@ export class FormProductComponent implements OnInit {
       image: [''],
       description: ['', [Validators.required]],
     });
-  }
-
-  saveProduct(event: Event) {
-    event.preventDefault();
-    console.log(this.form.valid);
-    if (this.form.valid) {
-      const product = this.form.value;
-      this.productService.createProduct(product).subscribe( newProduct => {
-        console.log(newProduct);
-        this.router.navigate(['./admin/products']);
-      });
-    } else {
-      console.log(this.form.errors);
-    }
-    console.log(this.form.value);
   }
 
   get priceField() {
